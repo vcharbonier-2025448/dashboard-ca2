104fb0dec844677b150b78b53d850c4b247c7dc6
Valeria Charbonier <2025448@student.cct.ie>
2025-12-26 22:34:51 +0000

    update 9

--- CA2_energy_dashboard.py
+++ CA2_energy_dashboard.py
@@ -48,7 +48,7 @@ def fmt(label, val):
     if pd.isna(val):
         return "N/A"
     else:
-        return f"{val:,.2f}*100%"
+        return f"{val:,.2f*100}%"
 
 a, b = st.columns(2)
 c, d = st.columns(2)
@@ -75,60 +75,3 @@ with col1:
 
 with col2:
     st.dataframe(summary, use_container_width=True)
-
-
-#Unnsupervised model
-
-  
-# fitting
-scalar.fit(df_ie)
-scaled_data = scalar.transform(df_ie)
-  
-# Importing PCA
-from sklearn.decomposition import PCA
-  
-# components = 2
-pca = PCA(n_components = 2, random_state = 42)
-pca.fit(scaled_data)
-x_pca = pca.transform(scaled_data)
-  
-# Instantiate the KMeans models
-
-k_results = []
-
-for k in range(2,8):
-    km = KMeans(n_clusters = k, random_state=42,n_init=10)
-    labels = km.fit_predict(x_pca)
-
-    # Calculate Silhoutte Score
-    score = silhouette_score(x_pca, labels, metric='euclidean')
-
-    k_results.append({"k" : k,
-                      "silhouette": score})
-
-
-k_results_df = pd.DataFrame(k_results)
-best_k = int(k_results_df.loc[k_results_df["silhouette"].idxmax(),"k"])
-print(f' The best value of k is: {best_k}')
-
-kmeans_final = KMeans (n_clusters = best_k, random_state = 42, n_init=10)
-cluster_labels = kmeans_final.fit_predict(x_pca)
-
-features = ["import_dependency","domestic_supply", "energy_balance", "renew_prod_%", "no_renew_prod_%"]
-x_unsup = df_ie[features].dropna().reset_index(drop=True)
-
-df_unsup = df_ie.loc[x_unsup.index].copy()
-
-df_unsup["cluster"] = cluster_labels
-
-df_unsup[["cluster"] + features].groupby("cluster").mean()
-
-plt.figure(figsize = (8,4))
-
-plt.scatter(x_pca[:,0], x_pca[:,1], c=df_unsup["cluster"], cmap = 'viridis' )
-
-plt.xlabel("First Principal Component")
-plt.ylabel("Second Principal Component")
-plt.title("KMeans Clusters in PCA Space")
-
-plt.show()
